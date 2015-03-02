a28f574627fcea19f4c3246d2a112049e808210b
Steve <scumdogsteev@users.noreply.github.com>
2015-03-02 13:27:40 -0600

    Update pkgInst.R

--- pkgInst.R
+++ pkgInst.R
@@ -1,15 +1,16 @@
 ## "pkgInst" is a function that checks whether a set of packages are 
-## installed and, if not, it installs them
+## installed and, if not, it installs them.  i is a set of packages.
+## 
 ## based on this response from Stack Overflow:
 ## http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them/19870272#19870272
 pkgInst <- function(x) {
-    for(i in x) {
+    for (i in x) {
         ## "require" returns TRUE invisibly if it was able to load package
-        if(!require(i, character.only = TRUE) ){
+        if (!require(i, character.only = TRUE)) {
              ##  If package was not able to be loaded then install it
              install.packages(i, dependencies = TRUE)
             ##  Load package after installing
-            require(i, character.only = TRUE)
+            require (i, character.only = TRUE)
         }
     }
 }
