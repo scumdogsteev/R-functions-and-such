bb612a67f076c186eb498fff5e74210933efe599
Steve <scumdogsteev@users.noreply.github.com>
2015-03-02 13:25:29 -0600

    Update pkgInst.R

--- pkgInst.R
+++ pkgInst.R
@@ -3,13 +3,13 @@
 ## based on this response from Stack Overflow:
 ## http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them/19870272#19870272
 pkgInst <- function(x) {
-for(i in x) {
-    ## "require" returns TRUE invisibly if it was able to load package
-    if(!require(i, character.only = TRUE) ){
-         ##  If package was not able to be loaded then install it
-         install.packages(i, dependencies = TRUE)
-         ##  Load package after installing
-         require(i, character.only = TRUE)
-         }
+    for(i in x) {
+        ## "require" returns TRUE invisibly if it was able to load package
+        if(!require(i, character.only = TRUE) ){
+             ##  If package was not able to be loaded then install it
+             install.packages(i, dependencies = TRUE)
+            ##  Load package after installing
+            require(i, character.only = TRUE)
+        }
     }
 }
