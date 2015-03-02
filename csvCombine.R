05ff1fa07638c25996982053e7dd9aedb616f793
Steve <scumdogsteev@users.noreply.github.com>
2015-03-02 16:45:29 -0600

    Create csvCombine.R

---
+++ csvCombine.R
@@ -0,0 +1,23 @@
+## csvCombine.R
+## by Steve Myles
+## 2 March 2015
+##
+## "csvCombine" creates a data frame from a user-specified directory's files.
+## user can specify how many files to combine; default is all in "directory."
+##
+## to do:  1) extend to other file types
+##         2) error-checking to exclude files of the wrong type
+
+csvCombine <- function(directory, id = NULL) {
+    ## create a list of files in "directory"
+    files_list <- list.files(directory, full.names=TRUE)
+    ## create an empty data frame
+    data <- data.frame()                             
+    ## if "id" is null (default behavior), reset id to the length of the
+    ## "files_list" vector (i.e., the number of files in "directory")
+    if (is.null(id)) { id <- length(files_list) } 
+    ## loop through the files, rbinding them together 
+    for (i in id) {
+        data <- rbind(data, read.csv(files_list[i]))
+    }
+}
