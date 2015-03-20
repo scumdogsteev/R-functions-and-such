427aa13df4340d50b44838e70df1698df559b5f6
Steve <scumdogsteev@users.noreply.github.com>
2015-03-20 11:26:55 -0500

    Update pkgInst.R

--- pkgInst.R
+++ pkgInst.R
@@ -1,11 +1,15 @@
 ## pkgInst.R
 ## by Steve Myles
 ## http://steve.mylesandmyles.info/
-## 2 March 2015
+## 2 March 2015 (updated 20 March 2015)
 ##
 ## "pkgInst" is a function that checks whether a set of packages are 
 ## installed and, if not, it installs them.  i is a set of packages.
 ## 
+## this function is called as follows:
+## pkgInst(c("pkg1", "pkg2", "pkg3",..., "pkgN"))
+## where pkg1 through pkgN are the names of the required packages.
+##
 ## almost completely based on this response from Stack Overflow:
 ## http://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them/19870272#19870272
 
