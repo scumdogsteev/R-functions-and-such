3fa6dab0f7f334b3fb1e8dbc75adfd9a4524e598
Steve <scumdogsteev@users.noreply.github.com>
2015-03-26 17:08:27 -0500

    Update pkgInst.R

--- pkgInst.R
+++ pkgInst.R
@@ -17,9 +17,9 @@ pkgInst <- function(x) {
     for (i in x) {
         ## "require" returns TRUE invisibly if it was able to load package
         if (!require(i, character.only = TRUE)) {
-            ##  If package was not able to be loaded then install it
+            ## if package was not able to be loaded, install it
             install.packages(i, dependencies = TRUE)
-            ##  Load package after installing
+            ## load package after installing
             require (i, character.only = TRUE)
         }
     }
