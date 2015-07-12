194c4ec726e65fc87458b40f6363ef68960750fc
Scumdogsteev <steve@mylesandmyles.info>
2015-07-11 19:32:15 -0500

    adding dozens

--- printMult/printMult.R
+++ printMult/printMult.R
@@ -1,7 +1,7 @@
 ## printMult
 ## by Steve Myles
 ## http://steve.mylesandmyles.info/
-## 4 June 2015
+## 11 July 2015
 ## documentation:  http://projects.mylesandmyles.info/R-functions-and-such/printMult/printMult.html
 ##
 ## The following function, `printCurrency`, properly formats numbers as 
@@ -9,7 +9,7 @@
 ##
 ## The following function, printMult, displays a number with an added 
 ## multiplier. multiplier is designed to allow for easy formatting of tens, 
-## hundreds, thousands, millions, or billions by displaying a 
+## dozens, hundreds, thousands, millions, or billions by displaying a 
 ## multiplier-specific letter after the formatted number. multiplier works for 
 ## values other than those listed but does not currently display a symbol after 
 ## the formatted number. The default is 1 (i.e., the number is displayed as 
@@ -18,11 +18,12 @@
 printMult <- function(value, digits = 2, sep = ",", decimal=".", multiplier = 1, 
                       mult.sym = "") {     
     mult.sym <- ifelse(multiplier == 10^1, " tens", 
-                       ifelse(multiplier == 10^2, "H",
-                              ifelse(multiplier == 10^3, "K",
-                                     ifelse(multiplier == 10^6, "M",
-                                            ifelse(multiplier == 10^9, "B", 
-                                                   "")))))
+                       ifelse(multiplier == 12, " doz.",
+                              ifelse(multiplier == 10^2, "H",
+                                     ifelse(multiplier == 10^3, "K",
+                                            ifelse(multiplier == 10^6, "M",
+                                                   ifelse(multiplier == 10^9, 
+                                                          "B", ""))))))
     paste(formatC(value / multiplier, format = "f", big.mark = sep, digits = digits, 
                   decimal.mark=decimal), mult.sym, sep="")
 }
